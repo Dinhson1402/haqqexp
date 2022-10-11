e80238113653ac46775c5c7a5b4486121e7b9786
tarabukinivan <56988566+tarabukinivan@users.noreply.github.com>
2022-10-11 09:25:06 +0900

    Create apival.js

---
+++ requests/apival.js
@@ -0,0 +1,32 @@
+const exec = require('child_process').exec;
+module.exports = async function(status){
+    this.execCommand = function (tmp) {
+        return new Promise((resolve) => {
+            if (status == 'active') {
+                tmp = "haqqd q staking validators -o json --limit=1000 \| jq \'\.validators[] \| select(\.status==\"BOND_STATUS_BONDED\")' \| jq -r \'\.tokens \+ \"\\t\" \+ \.description\.moniker \+ \"\\t\" \+ \.operator_address \+ \"\\t\" \+ \.commission\.commission_rates\.rate\' \| sort -gr \| nl";
+            }
+            else if (status == 'inactive'){
+                tmp = "haqqd q staking validators -o json --limit=1000 \| jq \'\.validators[] \| select(\.status==\"BOND_STATUS_UNBONDING\" or \.status==\"BOND_STATUS_UNBONDED\")' \| jq -r \'\.tokens \+ \"\\t\" \+ \.description\.moniker \+ \"\\t\" \+ \.operator_address \+ \"\\t\" \+ \.commission\.commission_rates\.rate\' \| sort -gr \| nl";
+            }
+        
+          
+          exec(tmp, (error, stdout, stderr) => {
+            if (error) {
+              console.error(`exec error: ${error}`);
+              
+              resolve(stderr);
+            }
+            if (stderr) {
+                console.error(`stderr: ${stderr}`);
+                
+                return;
+            }
+            resolve(stdout);
+          });
+          
+        });
+      };
+    
+    var lp = await this.execCommand()
+    return lp
+}
